de8f0edd0c54f7b47d361c1763212a58cc17acd8
Alexander Waya Nguku <alex.waya@gmail.com>
2022-06-04 18:02:11 +0300

    Adding an empty web map

--- markers/views.py
+++ markers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import TemplateView
+
+
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
